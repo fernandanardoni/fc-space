f1eda99a943f145a771395d74135a3331303da86
Fernanda Nardoni <thefray.br@gmail.com>
2021-09-06 19:48:58 -0300

    adicionando side-menu na página de agendamentos

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elem = document.querySelectorAll('.tabs');
     var instance = M.Tabs.init(elem);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
 });
