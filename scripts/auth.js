04136e31483fc99078e3c397a4d8c44143d499a5
Fernanda Nardoni <thefray.br@gmail.com>
2021-09-07 18:52:49 -0300

    test commit

--- scripts/auth.js
+++ scripts/auth.js
@@ -16,7 +16,7 @@ const userView = document.querySelector('.user-view');
 // ouvindo mudanças de login e logout
 auth.onAuthStateChanged(user => {
     if (user) {
-        console.log('user logged in', user)
+        console.log('user logged in', user.email)
         const userInfo = `<a href="#email"><span id="profile-email" class="black-text email">${user.email}</span></a>`;
         userView.innerHTML = userInfo;
     } else {
