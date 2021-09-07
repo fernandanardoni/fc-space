6b05b35f6f7cf2acc421e70838e15e680dba7b37
whenvolpi <69219632+whenvolpi@users.noreply.github.com>
2021-09-07 19:09:53 -0300

    Atualizações nas istâncias

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var registerTabs = document.querySelectorAll('.tabs');
-    var instance = M.Tabs.init(registerTabs);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var sideNav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sideNav);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
 });
@@ -27,3 +17,13 @@ const setSchedules = (data) => {
         schedulesCollection.innerHTML = html;
     })
 }
+document.addEventListener('DOMContentLoaded', function() {
+        var elemSelect = document.querySelectorAll('select');
+        var instanceSelect = M.FormSelect.init(elemSelect);
+        var elemDate = document.querySelectorAll('.datepicker');
+        var instanceDate = M.Datepicker.init(elemDate);
+        var elemTime = document.querySelectorAll('.timepicker');
+        var instanceTime = M.Timepicker.init(elemTime);
+    }
+
+);
