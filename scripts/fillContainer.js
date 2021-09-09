a0db797eb0279722d17ee6089d9fad6c957ca6b0
Hiago Macêdo <hiagomacedo03@hotmail.com>
2021-09-09 01:47:30 -0300

    Acessando os agendamentos do Banco de Dados
    Atualizei a tela de controle de agendamento para pegar e mostrar os agendamentos do Banco de Dados.

--- scripts/fillContainer.js
+++ scripts/fillContainer.js
@@ -1,32 +1,78 @@
 // Preencher os agendamentos
 
-const content = `
-<div class="schedule-container">
-<div class="info">
-    <p class="user">João Paulo</p>
-
-    <div class="schedule-day">
-        <p class="date">11/10/21</p>
-        <p class="hora">13:55h</p>
-    </div>
-
-    <p class="sector">Setor Leste</p>
-</div>
-
-<div class="actions">
-    <a href="./createSchedule.html" class="remarca">
-        <img src="assets/edit_black_24dp.svg" alt="remarcar">
-    </a>
-    <a href="#" class="delete" onclick=" openModal()">
-        <img src="assets/delete_black_24dp.svg" alt="apagar">
-    </a>
-</div>
-</div>
-`;
 var saoPauloUnit = document.getElementById('sao-paulo');
 
 var santosUnit = document.getElementById('santos');
 
-saoPauloUnit.innerHTML += content + content + content + content;
+const scheduleSantos = getSchedulesByFilial('Santos');
+const scheduleSaoPaulo = getSchedulesByFilial('Sao Paulo');
 
-santosUnit.innerHTML += content + content + content + content;
+function fillScheduleListSantos() {
+    scheduleSantos.then((unit) => {
+        var contentSantos = '';
+        for (i in unit) {
+            contentSantos += `
+        <div class="schedule-container">
+        <div class="info">
+            <p class="user">${unit[i].funcionario}</p>
+        
+            <div class="schedule-day">
+                <p class="date">${unit[i].data}</p>
+                <p class="hora">13:55h</p>
+            </div>
+        
+            <p class="sector">${unit[i].setor}</p>
+        </div>
+        
+        <div class="actions">
+            <a href="./createSchedule.html" class="remarca">
+                <img src="assets/edit_black_24dp.svg" alt="remarcar">
+            </a>
+            <a href="#" class="delete" onclick=" openModal()">
+                <img src="assets/delete_black_24dp.svg" alt="apagar">
+            </a>
+        </div>
+        </div>
+        `;
+        }
+
+        santosUnit.innerHTML += contentSantos;
+    });
+}
+
+fillScheduleListSantos();
+
+function fillScheduleListSaoPaulo() {
+    scheduleSaoPaulo.then((unit) => {
+        var contentSaoPaulo = '';
+        for (i in unit) {
+            contentSaoPaulo += `
+        <div class="schedule-container">
+        <div class="info">
+            <p class="user">${unit[i].funcionario}</p>
+        
+            <div class="schedule-day">
+                <p class="date">${unit[i].data}</p>
+                <p class="hora">13:55h</p>
+            </div>
+        
+            <p class="sector">${unit[i].setor}</p>
+        </div>
+        
+        <div class="actions">
+            <a href="./createSchedule.html" class="remarca">
+                <img src="assets/edit_black_24dp.svg" alt="remarcar">
+            </a>
+            <a href="#" class="delete" onclick=" openModal()">
+                <img src="assets/delete_black_24dp.svg" alt="apagar">
+            </a>
+        </div>
+        </div>
+        `;
+        }
+
+        saoPauloUnit.innerHTML += contentSaoPaulo;
+    });
+}
+
+fillScheduleListSaoPaulo();
