86675cb5a7c5ac60b49f02b8718df2dda6bb3e54
Bruno Araujo <brunoaraujo2395@gmail.com>
2021-09-10 15:29:11 -0300

    Inserindo os scripts do storage e ajustando o configDate

--- scripts/setSchedule.js
+++ scripts/setSchedule.js
@@ -5,23 +5,38 @@ const agendamentoSP = document.getElementById("filialSP");
 
 const agendamentoSantos = document.getElementById("filialSantos");
 
+const dados = getToken("token").split(",")
+console.log('dados', dados)
+
+let identificador;
+
+ db.collection("Usuario").doc(dados[1]).get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => console.log(doc))})
+console.log('variavel', identificador)
+
+// login
+// setToken([data.user.email, data.user.uid]);
 
 agendamentoSP.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
+    
+
+
     const newAgenda = {
         filial: "São Paulo",
         setor: agendamentoSP["section"].value,
         andar: agendamentoSP["andar"].value,
-        email: "roger@fcamara.com.br",
+        email: dados[0],
         funcionario: "Roger",
         cpf: "00586712003",
         data: agendamentoSP["data"].value
     }
 
-    
+    console.log('newAgenda', newAgenda)
 
+    
     db.collection("Agendamentos").add(newAgenda)
     .then(() => {
         alert("Agendamento adicionado!")
@@ -40,11 +55,13 @@ agendamentoSantos.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
+    const dados = getToken("token").split(",")
+
     const newAgenda = {
         filial: "Santos",
         setor: agendamentoSantos["section"].value,
         andar: 1,
-        email: "roger@fcamara.com.br",
+        email: dados[0],
         funcionario: "Roger",
         cpf: "00586712003",
         data: agendamentoSantos["date"].value
