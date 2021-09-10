38da2f934e05cee14cff322b409653c65a17c052
Fernanda Nardoni <thefray.br@gmail.com>
2021-09-10 17:46:09 -0300

    criando a lógica de armazenamento do agendamentos na coleçao do usuário

--- scripts/setSchedule.js
+++ scripts/setSchedule.js
@@ -1,70 +1,70 @@
 
+// const agendamentoSP = document.getElementById("filialSP");
 
-const agendamentoSP = document.getElementById("filialSP");
+// const agendamentoSantos = document.getElementById("filialSantos");
 
+// agendamentoSP.addEventListener("submit", (event) => {
 
-const agendamentoSantos = document.getElementById("filialSantos");
+//     event.preventDefault();
 
+//     const newAgenda = {
+//         filial: "São Paulo",
+//         setor: agendamentoSP["section"].value,
+//         andar: agendamentoSP["andar"].value,
+//         // email: "roger@fcamara.com.br",
+//         // funcionario: "Roger",
+//         // cpf: "00586712003",
+//         // data: agendamentoSP["data"].value
+//     }
 
-agendamentoSP.addEventListener("submit", (event) => {
-
-    event.preventDefault();
-
-    const newAgenda = {
-        filial: "São Paulo",
-        setor: agendamentoSP["section"].value,
-        andar: agendamentoSP["andar"].value,
-        email: "roger@fcamara.com.br",
-        funcionario: "Roger",
-        cpf: "00586712003",
-        data: agendamentoSP["data"].value
-    }
-
-    
-
-    db.collection("Agendamentos").add(newAgenda)
-    .then(() => {
-        alert("Agendamento adicionado!")
-        setTimeout(function() {
-            window.location.href = "seeSchedules.html";
-        }, 2000);
-    })
-    .catch((error) => {
-        console.error("Ocorreu um erro ao adicionar o agendamento: ", error);
-    });
-
-})
+//     const userId = db.collection('Usuario').doc(user.uid)
+//     console.log(userId)
 
 
-agendamentoSantos.addEventListener("submit", (event) => {
 
-    event.preventDefault();
 
-    const newAgenda = {
-        filial: "Santos",
-        setor: agendamentoSantos["section"].value,
-        andar: 1,
-        email: "roger@fcamara.com.br",
-        funcionario: "Roger",
-        cpf: "00586712003",
-        data: agendamentoSantos["date"].value
-    }
+//     // db.collection("Agendamentos").add(newAgenda)
+//     // .then(() => {
+//     //     alert("Agendamento adicionado!")
+//     //     setTimeout(function() {
+//     //         window.location.href = "seeSchedules.html";
+//     //     }, 2000);
+//     // })
+//     // .catch((error) => {
+//     //     console.error("Ocorreu um erro ao adicionar o agendamento: ", error);
+//     // });
 
- 
+// })
 
-    db.collection("Agendamentos").add(newAgenda)
-    .then(() => {
+// agendamentoSantos.addEventListener("submit", (event) => {
 
-        alert("Agendamento adicionado!")
-        setTimeout(function() {
-            window.location.href = "seeSchedules.html";
-        }, 2000);
+//     event.preventDefault();
 
-    })
-    .catch((error) => {
-        console.error("Ocorreu um erro ao adicionar o agendamento: ", error);
-    });
+//     const newAgenda = {
+//         filial: "Santos",
+//         setor: agendamentoSantos["section"].value,
+//         andar: 1,
+//         email: "roger@fcamara.com.br",
+//         funcionario: "Roger",
+//         cpf: "00586712003",
+//         data: agendamentoSantos["date"].value
+//     }
 
-})
+
+
+//     db.collection("Agendamentos").add(newAgenda)
+//         .then(() => {
+
+//             alert("Agendamento adicionado!")
+//             setTimeout(function () {
+//                 window.location.href = "seeSchedules.html";
+//             }, 2000);
+
+//         })
+//         .catch((error) => {
+//             console.error("Ocorreu um erro ao adicionar o agendamento: ", error);
+//         });
+
+// })
 
 
