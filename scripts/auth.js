cc485935f07fdfa4c3b3999768e31e5c46c128eb
Fernanda Nardoni <thefray.br@gmail.com>
2021-09-10 21:19:11 -0300

    adicionando agendamentos do usuário na tela de controle

--- scripts/auth.js
+++ scripts/auth.js
@@ -8,8 +8,9 @@ auth.onAuthStateChanged(user => {
         db.collection('Usuario').get().then(snapshots => {
             snapshots.forEach((doc) => {
                 if (doc.id == user.uid) {
+                    console.log(doc.data())
                     const userInfo = `
-                        <a href="#email"><span id="profile-email" class="black-text email">${doc.data().nome}</span></a>
+                        <a href="#email"><span id="profile-email" class="black-text email">${doc.data().nome || doc.data().name}</span></a>
                         <a href="#email"><span id="profile-email" class="black-text email">${doc.data().email}</span></a>`;
                     userView.innerHTML = userInfo;
                 };
