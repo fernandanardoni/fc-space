079720686ecd6d14595de2f654d0e4a54a7e8552
Hiago Macêdo <hiagomacedo03@hotmail.com>
2021-09-14 18:38:22 -0300

    Update getSchedule.js

--- scripts/getSchedule.js
+++ scripts/getSchedule.js
@@ -1,78 +1,3 @@
-
-
-// //Pegas os agendamentos no banco de dados e retorna um Array;
-
-// function getSchedulesByFilial(filial) {
-//     const dataCollection = db.collection('Agendamentos');
-//     return dataCollection.get().then((item) => {
-//         const schedule = [];
-//         item.forEach((data) => {
-//             if (data.data().filial == filial) {
-//                 schedule.push(data.data());
-//             }
-//         });
-//         return schedule;
-//     });
-// }
-
-// // Pegando todos os agendamentos
-
-// function getSchedules() {
-//     const datasCollection = db.collection('Agendamentos');
-//     datasCollection.get().then((item) => {
-//         item.forEach((data) => {
-//             console.log('agendamentos', data.data());
-//         });
-//     });
-// }
-
-// // Pegando uma informação dentro da tabela de usuário
-
-// function getUser() {
-//     const users = db
-//         .collection('Usuario')
-//         .doc('lC9klqnuJuUv5ylIwNNU')
-//         .collection('agendamentos');
-
-//     users.get().then(function (querySnapshot) {
-//         querySnapshot.forEach(function (doc) {
-//             console.log('Usuario', doc.data());
-//         });
-//     });
-// }
-
-// // Pegando apenas uma filial
-
-// function getOffice() {
-
-//     // [START get_all_users]
-//     db.collection("Escritorio").get().then((querySnapshot) => {
-//         querySnapshot.forEach((doc) => {
-//             console.log(`${doc.id} => ${Object.keys(doc.data())}`);
-//         });
-//     });
-
-//     // const docRef = db.collection('Escritorio').doc("1");
-
-//     // docRef
-//     //     .get()
-//     //     .then((doc) => {
-//     //         if (doc.exists) {
-//     //             console.log('dados da filial:', doc.data());
-//     //         } else {
-//     //             // doc.data() will be undefined in this case
-//     //             console.log('No such document!');
-//     //         }
-//     //     })
-//     //     .catch((error) => {
-//     //         console.log('Error getting document:', error);
-//     //     });
-// }
-
-// getSchedules();
-// getUser();
-// getOffice();
-
 const schedulesList = document.querySelector('.schedule-content');
 
 const getScheduleByUser = () => {
@@ -102,10 +27,14 @@ const getScheduleByUser = () => {
 
                     snapshots.forEach((doc) => {
 
-                        monthSelector(doc.data().data, doc.data().filial, doc.id);
+                        monthSelector(doc.data().data, doc.data().filial, doc.id, doc.data().andar);
                     });
 
-                    schedulesList.innerHTML = getFinalHTML();
+                    // schedulesList.innerHTML = getFinalHTML();
+
+                    schedulesList.innerHTML = getFinalHTML()? getFinalHTML() : '<h2 style = "margin-left: 16px; ; margin-top: 10px; font-size: 20px">Sem Agendamentos</h2>';
+
+                    
 
                 });
         } else {
@@ -174,11 +103,11 @@ function deleteSchedule(idToDelete, newPage) {
 }
 
 // Separa os agendamentos por mês
-function monthSelector(date, filial, id) {
+function monthSelector(date, filial, id, floor) {
 
     let content = `
     <li class="collection-item">
-    <p class="sector">${filial}</p>
+    <p class="sector">${filial} - ${floor} andar</p>
 
     <p class="date">${getDayofWeek(date)} - ${date}</p>
 
