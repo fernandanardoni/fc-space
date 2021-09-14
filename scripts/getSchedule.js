58a01984faef855e542c60bd54f5688a26366560
Hiago Macêdo <hiagomacedo03@hotmail.com>
2021-09-14 12:44:20 -0300

    Update layout

--- scripts/getSchedule.js
+++ scripts/getSchedule.js
@@ -71,17 +71,10 @@
 // getUser();
 // getOffice();
 
-const schedulesList = document.querySelector('#schedules-list');
+const schedulesList = document.querySelector('.schedule-content');
 
 const getScheduleByUser = () => {
 
-    let month = document.querySelector(".month");
-    console.log("mes", month);
-    
-    if (month) {
-        month.innerHTML = ""
-    }
-    
     auth.onAuthStateChanged((user) => {
         if (user) {
             db.collection('Usuario')
@@ -89,40 +82,27 @@ const getScheduleByUser = () => {
                 .collection('agendamentos')
                 .get()
                 .then((snapshots) => {
-                    let html = '';
+                    months = {
+                        jan: [],
+                        fev: [],
+                        mar: [],
+                        abr: [],
+                        mai: [],
+                        jun: [],
+                        jul: [],
+                        ago: [],
+                        set: [],
+                        out: [],
+                        nov: [],
+                        dez: [],
+                    }
+                    
                     snapshots.forEach((doc) => {
-                        console.log(doc.data());
+                            monthSelector(doc.data().data, doc.data().filial, doc.id);
+                        });
 
-                        const scheduleItem = `
-                        <li class="collection-item">
-                        
-                 <p class="sector">${doc.data().filial}</p>
-                 </div>
+                            schedulesList.innerHTML = getFinalHTML();
 
-                 <p class="date">${doc.data().data}</p>
-                 </div>
-
-                 <div class="actions">
-
-
-
-                 <a href="#" id="${doc.id}" onclick="openModal(this.id)">
-                    <img src="assets/delete-icon.svg" alt="delete">
-                </a>
-                <a href="#" id="">
-                    <img src="assets/edit-icon.svg" alt="edit">
-                </a>
-                
-                </div>
-                        </li>
-                    `;
-
-                        html += scheduleItem;
-
-                        schedulesList.innerHTML = html;
-                    });
-
-                schedulesList.insertAdjacentHTML("beforebegin", `<h2 class="month" >Setembro</h2>`)
                 });
         } else {
             console.log('user logged out');
@@ -149,13 +129,234 @@ function closeModal() {
 //função pra excluir um agendamento.
 
 function deleteSchedule(idToDelete) {
-    
+
     auth.onAuthStateChanged((user) => {
         if (user) {
             db.collection('Usuario').doc(user.uid).collection('agendamentos').doc(idToDelete).delete();
 
         }
     });
+
+    console.log("id deletado", idToDelete)
     closeModal();
-    getScheduleByUser();
+
+    setTimeout(function(){
+        getScheduleByUser();
+    }, 350);
+
+}
+
+
+// Separa os agendamentos por mês
+function monthSelector(date, filial, id) {
+    
+    let content = `
+    <li class="collection-item">
+    <p class="sector">${filial}</p>
+
+    <p class="date">${date}</p>
+
+    <div class="actions">
+        <a href="#" id="${id}" onclick="openModal(this.id)">
+            <img src="assets/delete-icon.svg" alt="delete">
+        </a>
+        <a href="#" id="">
+            <img src="assets/edit-icon.svg" alt="edit">
+        </a>
+
+    </div>
+    </li>
+    `
+
+    if (date.includes("/01/")) {
+        months.jan.push(content);
+    } else if (date.includes("/02/")) {
+        months.fev.push(content);
+    } else if (date.includes("/03/")) {
+        months.mar.push(content);
+    } else if (date.includes("/04/")) {
+        months.abr.push(content);
+    } else if (date.includes("/05/")) {
+        months.mai.push(content);
+    } else if (date.includes("/06/")) {
+        months.jun.push(content);
+    } else if (date.includes("/07/")) {
+        months.jul.push(content);
+    } else if (date.includes("/08/")) {
+        months.ago.push(content);
+    } else if (date.includes("/09/")) {
+        months.set.push(content);
+    } else if (date.includes("/10/")) {
+        months.out.push(content);
+    } else if (date.includes("/11/")) {
+        months.nov.push(content);
+    } else if (date.includes("/12/")) {
+        months.dez.push(content);
+    }
+}
+
+var months = {
+    jan: [],
+    fev: [],
+    mar: [],
+    abr: [],
+    mai: [],
+    jun: [],
+    jul: [],
+    ago: [],
+    set: [],
+    out: [],
+    nov: [],
+    dez: [],
+}
+
+function getFinalHTML() {
+    let finalHTML = '';
+
+    if (months.jan.length > 0) {
+        finalHTML += `
+            <h2 class="month">Janeiro</h2>
+            <ul class="collection" id="schedules-list">
+            `
+
+        months.jan.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.fev.length > 0) {
+        finalHTML += `
+        <h2 class="month">Fevereiro</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.fev.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.mar.length > 0) {
+        finalHTML += `
+        <h2 class="month">Março</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.mar.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.abr.length > 0) {
+        finalHTML += `
+        <h2 class="month">Abril</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.abr.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.mai.length > 0) {
+        finalHTML += `
+        <h2 class="month">Maio</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.mai.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.jun.length > 0) {
+        finalHTML += `
+        <h2 class="month">Junk</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.jun.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.jul.length > 0) {
+        finalHTML += `
+        <h2 class="month">Julho</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.jul.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.ago.length > 0) {
+        finalHTML += `
+        <h2 class="month">Agosto</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.ago.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.set.length > 0) {
+        finalHTML += `
+            <h2 class="month">Setembro</h2>
+            <ul class="collection" id="schedules-list">
+            `
+
+        months.set.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.out.length > 0) {
+        finalHTML += `
+        <h2 class="month">Outubro</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.out.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.nov.length > 0) {
+        finalHTML += `
+        <h2 class="month">Novembro</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.nov.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+    if (months.dez.length > 0) {
+        finalHTML += `
+        <h2 class="month">Dezembro</h2>
+        <ul class="collection" id="schedules-list">
+        `
+
+        months.dez.forEach((li) => {
+            finalHTML += li;
+        })
+
+        finalHTML += `</ul>`
+    }
+
+    return finalHTML;
 }
